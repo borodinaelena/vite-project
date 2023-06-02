3638f1c80fa8f3e05be656e00e43b200c0398e72
Olena <olena.oplachko@gmail.com>
2023-06-02 14:18:34 +0300

    create simple main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Layout from './components/Layout/index.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <h1>Vite + React</h1>
-      <div className="dropdown">
-        <label tabIndex="0" className="btn m-1">Click</label>
-        <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-          <li><a>Item 1</a></li>
-          <li><a>Item 2</a></li>
-        </ul>
-      </div>
-
-
-      <label className="swap">
-
-        {/* this hidden checkbox controls the state */}
-        <input type="checkbox" />
-
-        {/* volume on icon */}
-        <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"/></svg>
-
-        {/* volume off icon */}
-        <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"/></svg>
-
-      </label>
-      <div tabIndex={0} className="collapse bg-base-200">
-        <div className="collapse-title text-xl font-medium">
-          Focus me to see content
-        </div>
-        <div className="collapse-content">
-          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-        </div>
-      </div>
-
-      <span className="loading loading-spinner loading-xs"></span>
-      <span className="loading loading-spinner loading-sm"></span>
-      <span className="loading loading-spinner loading-md"></span>
-      <span className="loading loading-spinner loading-lg"></span>
-      <span className="loading loading-spinner text-primary"></span>
-      <span className="loading loading-spinner text-secondary"></span>
-      <span className="loading loading-spinner text-accent"></span>
-      <span className="loading loading-spinner text-neutral"></span>
-      <span className="loading loading-spinner text-info"></span>
-      <span className="loading loading-spinner text-success"></span>
-      <span className="loading loading-spinner text-warning"></span>
-      <span className="loading loading-spinner text-error"></span>
-
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout />
   )
 }
 
